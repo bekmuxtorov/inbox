51dd6cdf0b3c717a67bd3f0346e4c3b229969756
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2025-02-02 22:30:59 +0500

    📜 Updated websocket consumer functions

--- backend/apps/tickets/consumers.py
+++ backend/apps/tickets/consumers.py
@@ -1,34 +1,72 @@
 import json
+from datetime import datetime as dt
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import Ticket, Message, UserDetails
+from django.contrib.auth.models import User
+from channels.db import database_sync_to_async
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.ticket_id = self.scope["url_route"]["kwargs"]["ticket_uuid"]
+        self.ticket_group_name = f"chat_{self.ticket_id}"
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        ticket, _ = await Ticket.objects.aget_or_create(ticket_uuid=self.ticket_id)
+        self.ticket = ticket
 
-        self.accept()
+        await self.channel_layer.group_add(self.ticket_group_name, self.channel_name)
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.ticket_group_name, self.channel_name)
+        await self.set_user_offline()
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        message_text = text_data_json.get("message", "").strip()
+        time = text_data_json.get("time", "")
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
+        if not message_text:
+            return
+
+        await self.save_message(message_text)
+
+        await self.channel_layer.group_send(
+            self.ticket_group_name,
+            {
+                "type": "chat.message",
+                "message": message_text,
+                "user": await self.get_user_details(),
+                "time": time
+            },
         )
 
-    def chat_message(self, event):
-        message = event["message"]
+    @database_sync_to_async
+    def set_user_offline(self):
+        user = UserDetails.objects.filter(id=self.scope["user"].id).first()
+        if user:
+            user.status = "offline"
+            user.save()
 
-        self.send(text_data=json.dumps({"message": message}))
+    @database_sync_to_async
+    def get_user(self):
+        return self.scope["user"]
+
+    @database_sync_to_async
+    def get_user_details(self):
+        return self.ticket.user_details
+
+    async def save_message(self, message_text):
+        user = await self.get_user()
+
+        await Message.objects.acreate(
+            sender_operator=user if isinstance(user, User) else None,
+            sender_guest=await self.get_user_details() if not isinstance(user, User) else None,
+            ticket=self.ticket,
+            text=message_text,
+        )
+
+    async def chat_message(self, event):
+        user = event["user"]
+        await self.send(text_data=json.dumps({"message": event["message"], "sender": user.username, "time": event["time"]}))
